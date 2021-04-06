1a1b14a95a2b34c0aee38a3baf84a9c2a816a8ed
unknown <felipelauton021@gmail.com>
2021-04-06 16:19:14 -0300

    segurança da api

---
+++ blogPessoal/blogPessoal-api/src/main/java/generation/org/blogPessoal/controller/UsuarioController.java
@@ -0,0 +1,39 @@
+package generation.org.blogPessoal.controller;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import generation.org.blogPessoal.model.UserLogin;
+import generation.org.blogPessoal.model.Usuario;
+import generation.org.blogPessoal.service.UsuarioService;
+
+@RestController
+@RequestMapping("/usuarios")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
+public class UsuarioController {
+	
+	@Autowired
+	private UsuarioService usuarioService;
+
+	@PostMapping("/logar")
+	public ResponseEntity<UserLogin> Autentication(@RequestBody Optional<UserLogin> user){
+		return usuarioService.Logar(user).map(resp -> ResponseEntity.ok(resp))
+				.orElse(ResponseEntity.status(HttpStatus.UNAUTHORIZED).build());
+	}
+	
+	@PostMapping("/cadastrar")
+	public ResponseEntity<Usuario> Post (@RequestBody Usuario usaurio){
+		return ResponseEntity.status(HttpStatus.CREATED)
+				.body(usuarioService.CadastrarUsuario(usaurio));
+	}
+	
+	
+}
